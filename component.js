d7705387a6e378acf7f229585edf0ac32188181d
Matthew <matbowrs@gmail.com>
2019-04-05 00:11:35 -0400

    Review form with basic form validation and show the reviews (if there are any). This was done with a product-review component. CSS was updated as well.

--- component.js
+++ component.js
@@ -51,16 +51,33 @@ Vue.component('product', {
         
         <button v-on:click="addToCart" 
                 :disabled="!inStock" 
-                :class="{ disabledButton: !inStock }"> Add to Cart
+                :class="{ disabledButton: !inStock }"
+                > 
+        Add to Cart
         </button>
 
         <button @click="removeFromCart"
                 :disabled="!inStock"
                 :class="{ disabledButton: !inStock }">Remove from Cart
         </button>
+        
 
     </div>
+    <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+            <li v-for="review in reviews">
+                <p> {{ review.name }} </p>
+                <p> {{ review.review }} </p>
+                <p> {{ review.rating }} </p>
+            </li>
+        </ul>
+    </div>
+    <product-review @review-submitted="addReview"></product-review>
+
 </div>
+
     `,
     data() {
         return {
@@ -84,7 +101,8 @@ Vue.component('product', {
                     variantQuantity: 30,
                     variantOnSale: false
                 }
-            ]
+            ],
+            reviews: []
         }
     }, 
     methods: {
@@ -108,6 +126,9 @@ Vue.component('product', {
         */
         updateProduct(index) {
             this.selectedVariant = index;
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview);
         }
     },
     computed: {
@@ -135,6 +156,82 @@ Vue.component('product', {
         }
     }
     
+})
+
+Vue.component('product-review', {
+    template: `
+        <!-- @submit.prevent prevents the page from refreshing -->
+
+        <form class="review-form" @submit.prevent="onSubmit">
+
+
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+
+            <ul>
+                <li v-for="error in errors"> {{ error }}</li>
+            </ul>
+        </p>
+
+    
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name">
+        </p>
+
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+
+        <p>
+            <label for="rating">Rating</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+
+
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+            } else {
+                if(!this.name)
+                    this.errors.push("Name required.");
+                if (!this.review)
+                    this.errors.push("Review required.");
+                if (!this.rating)
+                    this.errors.push("Rating required.");
+
+            }
+        }
+    }
 })
 
 const app = new Vue ({
